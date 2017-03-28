7b0e77ca67b9842a0b27b049d5c49e649f3934cb
Daniele Cappuccio <daniele97bsk@gmail.com>
2017-03-28 15:18:24 +0200

    problem 324

---
+++ factorial_frequencies.py
@@ -0,0 +1,22 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n-1)
+
+
+while (True):
+    inp = input();
+    if inp == "0":
+        break
+    n = int(inp)
+    print(n, "! --", sep = '')
+    fact = str(factorial(n))
+    print("   (0)    ", fact.count(str(0)), sep = '', end = '')
+    for i in range(1, 5):
+        print("    (", i, ")    ", fact.count(str(i)), sep = '', end = '')
+    print("\n", end = '')
+    print("   (5)    ", fact.count(str(5)), sep = '', end = '')
+    for j in range(6, 10):
+        print("    (", j, ")    ", fact.count(str(j)), sep = '', end = '')
+    print("\n", end = '')
+    
