6176583aab38449c8862f79cfe643686c4a865b1
Daniele <daniele97bsk@gmail.com>
2017-04-15 14:52:18 +0200

    problem 11727

---
+++ cost_cutting.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main(){	
+	int N;
+	cin >> N;
+	vector<int> V(3);
+	for (int i=0; i<N; ++i){
+		int a, b, c;
+		cin >> a >> b >> c;
+		V[0] = a; V[1] = b; V[2] = c;
+		sort(V.begin(), V.end());
+		cout << "Case " << i+1 << ": " << V[1] << endl;
+	}	
+	return 0;
+}
