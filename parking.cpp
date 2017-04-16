dd756b2943a3211f8729e4fd56c3300a0b62f9a5
Daniele <daniele97bsk@gmail.com>
2017-04-16 03:12:24 +0200

    problem 11364

---
+++ parking.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int N;
+	cin >> N;
+	for (int i=0; i<N; ++i){
+			int M, sum = 0;
+			int min = 100, max = 0;
+			cin >> M;
+			for (int j=0; j<M; ++j){
+				int t;
+				cin >> t;
+				sum += t;
+				if (t > max) max = t;
+				if (t < min) min = t;
+			}
+			int opt_park = (int) sum/M;
+			int r = 2*(opt_park - min) + 2*(max - opt_park);
+			cout << r << endl;
+	}
+	return 0;
+}
