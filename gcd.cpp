8675641ab811f8f645039abbbe6fe67c1e486881
Daniele <daniele97bsk@gmail.com>
2017-04-16 02:25:15 +0200

    problem 11417

---
+++ gcd.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int gcd(int i, int j){
+	if (i==1 || j==1) return 1;
+	if (i%j == 0) return j;
+	if (j%i == 0) return i;
+	if (i > j) return gcd(i-j, j);
+	return gcd(i, j-i);
+}
+
+int main(){
+	int N;
+	while (1){
+		cin >> N;
+		if (!N) break;
+		int G = 0;
+		for (int i=1; i<N; ++i){
+			for (int j=i+1; j<=N; ++j){
+				G += gcd(i,j);
+			} 
+		}
+		cout << G << endl;
+	}
+	return 0;
+}
