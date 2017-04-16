1b44b0ec69caf12fff0e069a266db88212d1caca
Daniele <daniele97bsk@gmail.com>
2017-04-16 11:58:59 +0200

    problem 11461

---
+++ square_numbers.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <tgmath.h>
+
+using namespace std;
+
+bool is_perfect_square(int n) {
+    if (n < 0)
+        return false;
+    int root(round(sqrt(n)));
+    return n == root * root;
+}
+
+int main(){
+	int a, b;
+	while (1){
+		cin >> a >> b;
+		if (!a && !b) break;
+		if (a > b){
+			int temp = b;
+			b = a;
+			a = temp;
+		}
+		int R = 0;
+		for (int i=a; i<=b; ++i){
+			if (is_perfect_square(i)) R++;
+		}
+		cout << R << endl;
+	}
+	return 0;
+}
