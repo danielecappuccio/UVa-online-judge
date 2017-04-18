e0caf4b4b06705b48a8d05e420c086a0f14809b9
Daniele <daniele97bsk@gmail.com>
2017-04-18 23:28:08 +0200

    problem 499

---
+++ whats_the_frequency_kenneth.py
@@ -0,0 +1,25 @@
+dic = {}
+while True:
+	try:
+		dic.clear()
+		line = input()
+		for c in line:
+			if c in "abcdefghijklmnopqrstuvwxyz" or c in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+				if c in dic:
+					dic[c] += 1
+				else:
+					dic[c] = 1
+		max_freq = 0
+		for key in dic:
+			if dic[key] > max_freq:
+				max_freq = dic[key]
+		max_freq_list = []
+		for key in dic:
+			if dic[key] == max_freq:
+				max_freq_list.append(key)
+		max_freq_list.sort()
+		for elem in max_freq_list:
+			print(elem, end="")
+		print(" " + str(max_freq))
+	except EOFError:
+		break
