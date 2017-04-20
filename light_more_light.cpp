4b6350d95c048b1a3329521051d4ced667a7e939
Daniele <daniele97bsk@gmail.com>
2017-04-20 22:11:24 +0200

    problem 10110

---
+++ light_more_light.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    unsigned int n;
+    unsigned int x;
+    
+    while(1){
+        scanf("%u",&n);
+        if(!n) break;
+        
+        x = sqrt(n);
+        
+        if(n == x*x) printf("yes\n");
+        else printf("no\n");
+    }   
+    return 0;
+}
