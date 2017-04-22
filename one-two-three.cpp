f616211b2e617dc23f328fe07e257f89620fe196
Daniele <daniele97bsk@gmail.com>
2017-04-22 18:02:40 +0200

    problem 12289

---
+++ one-two-three.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+
+    int N, count;
+    string l;
+    char one[] = {'o', 'n', 'e'};
+  
+    cin >> N;
+  
+    for(int i = 0; i < N; i++) {
+        cin >> l;
+        if(l.length() == 5) {
+            cout << 3 << endl;
+        }
+        else {
+            count = 0;
+            for(int j = 0; j < 3; j++) {
+                if(l[j] == one[j]) count++;
+            }
+            if(count < 2) cout << 2 << endl;
+            else cout << 1 << endl;
+        }
+    }
+    return 0;
+}
