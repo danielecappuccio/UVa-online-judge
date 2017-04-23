3ca42db976c08b8ec37102fa1729876edaaed06a
Daniele <daniele97bsk@gmail.com>
2017-04-23 15:39:13 +0200

    problem 11044

---
+++ searching_for_nessy.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int N, r, c, internal_r, internal_c, sonars;
+    cin >> N;
+    for (int i=0; i<N; ++i){
+        sonars = 0;
+        cin >> r >> c;
+        internal_r = r-2;
+        internal_c = c-2;
+        for (int j=0; j<((internal_r-1)/3)+1; ++j){
+            for (int k=0; k<((internal_c-1)/3)+1; ++k){
+                sonars++;
+            }
+        }
+        cout << sonars << endl;
+    }
+    return 0;
+}
