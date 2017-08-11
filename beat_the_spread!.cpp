1e05b2d8e0db13d423680c3f91be4d93710d3cd1
Daniele <daniele97bsk@gmail.com>
2017-08-11 17:43:39 +0200

    problem 10812

---
+++ beat_the_spread!.cpp
@@ -0,0 +1,23 @@
+/*
+ * Competitive Programming
+ *
+ * @author        Daniele Cappuccio
+ * @link          (https://github.com/daniele-cappuccio/UVa-online-judge)
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int s, d, T, t;
+    cin >> T;
+    for (t=0; t<T; ++t){
+        cin >> s >> d;
+        if (s < d || (s + d) % 2 == 1) cout << "impossible" << endl;
+        else {
+            cout << (s+d)/2 << " " << (s-d)/2 << endl;
+        }
+    }
+    return 0;
+}
