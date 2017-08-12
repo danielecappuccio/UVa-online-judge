04f9488258b955b9fc67e93996585094134dabce
Daniele <daniele97bsk@gmail.com>
2017-08-12 11:26:09 +0200

    problem 344

--- roman_digititis.c
+++ roman_digititis.c
@@ -1,3 +1,11 @@
+/*
+ * Competitive Programming
+ *
+ * @author        Daniele Cappuccio
+ * @link          (https://github.com/daniele-cappuccio/UVa-online-judge)
+ * @license       MIT License (https://opensource.org/licenses/MIT)
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
