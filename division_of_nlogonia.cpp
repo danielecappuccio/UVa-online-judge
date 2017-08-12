7e39cc31915df483db3b7bc91a3dd965844ee91f
Daniele <daniele97bsk@gmail.com>
2017-08-12 11:43:52 +0200

    problem 11498

--- division_of_nlogonia.cpp
+++ division_of_nlogonia.cpp
@@ -1,24 +1,33 @@
+/*
+ * Competitive Programming
+ *
+ * @author        Daniele Cappuccio
+ * @link          (https://github.com/daniele-cappuccio/UVa-online-judge)
+ * @license       MIT License (https://opensource.org/licenses/MIT)
+ */
+
 #include <iostream>
 
 using namespace std;
 
 int main() {
 	
-  int K, n, m, x, y;
+    int K, n, m, x, y;
   
-  while(true) {
-    cin >> K;
-    if(!K) break;
+    while(true) {
+        cin >> K;
+        if(!K) break;
     
-    cin >> n >> m;
-    for(int i = 0; i < K; i++) {
-      cin >> x >> y;
-      if(x == n || y == m) cout << "divisa" << endl;
-      else if(x > n && y > m) cout << "NE" << endl;
-      else if(x > n && y < m) cout << "SE" << endl;
-      else if(x < n && y < m) cout << "SO" << endl;
-      else if(x < n && y > m) cout << "NO" << endl;
+        cin >> n >> m;
+        for(int i = 0; i < K; i++) {
+            cin >> x >> y;
+            if(x == n || y == m) cout << "divisa" << endl;
+            else if(x > n && y > m) cout << "NE" << endl;
+            else if(x > n && y < m) cout << "SE" << endl;
+            else if(x < n && y < m) cout << "SO" << endl;
+            else if(x < n && y > m) cout << "NO" << endl;
+        }
     }
-  }
-  return 0;
+
+    return 0;
 }
