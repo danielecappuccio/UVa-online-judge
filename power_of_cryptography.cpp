d3436e4049c2f96774351b71576da6c5a12f2611
Daniele <daniele97bsk@gmail.com>
2017-08-13 17:57:35 +0200

    problem 113

---
+++ power_of_cryptography.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    double n,p;
+    double ans;
+
+    while(scanf("%lf%lf",&n,&p) == 2){
+        ans = pow(p,1/n);
+        printf("%.0lf\n",ans);
+    }
+
+    return 0;
+}
