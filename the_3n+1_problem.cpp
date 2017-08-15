a3997761247a00a133971bf0d81d27a639087c41
Daniele Cappuccio <daniele97bsk@gmail.com>
2017-08-15 21:44:40 +0200

    problem 100

--- the_3n+1_problem.cpp
+++ the_3n+1_problem.cpp
@@ -1,10 +1,17 @@
+/*
+ * Competitive Programming
+ *
+ * @author        Daniele Cappuccio
+ * @link          (https://github.com/daniele-cappuccio/UVa-online-judge)
+ * @license       MIT License (https://opensource.org/licenses/MIT)
+ */
+
 #include <cstdio>
 #include <algorithm>
  
 using namespace std;
  
-int main ()
-{
+int main (){
     int i, j;
  
     while ( scanf("%d %d", &i, &j) != EOF ) {
