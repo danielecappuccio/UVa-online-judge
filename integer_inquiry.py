f38993c47705f6d7c35c3e35b27beb1ace398121
Daniele <daniele97bsk@gmail.com>
2017-08-17 19:05:41 +0200

    problem 424

---
+++ integer_inquiry.py
@@ -0,0 +1,16 @@
+#
+# Competitive Programming
+#
+# @author        Daniele Cappuccio
+# @link          (https://github.com/daniele-cappuccio/UVa-online-judge)
+# @license       MIT License (https://opensource.org/licenses/MIT)
+#
+
+S = 0
+while True:
+    integer = int(input())
+    if integer == 0:
+        break
+    S += integer
+
+print(S)
