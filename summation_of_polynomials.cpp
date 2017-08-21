19ee97904752bdec6d9d5a8cd2df78e227202299
Daniele <daniele97bsk@gmail.com>
2017-08-22 01:09:17 +0200

    problem 10302

---
+++ summation_of_polynomials.cpp
@@ -0,0 +1,27 @@
+/*
+ * Competitive Programming
+ *
+ * @author        Daniele Cappuccio
+ * @link          (https://github.com/daniele-cappuccio/UVa-online-judge)
+ * @license       MIT License (https://opensource.org/licenses/MIT)
+ */
+
+#include <iostream> 
+#include <cstdio> 
+
+using namespace std;
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    int N; long long int res;
+
+    while (scanf("%d", &N) == 1){
+        res = 1;
+        res = res * N * (N + 1) / 2;
+        res = res * N * (N + 1) / 2;
+        cout << res << endl;
+    }
+
+    return 0;
+}
