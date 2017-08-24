acab70f01fba4bab801b83af48970e9562883545
Daniele <daniele97bsk@gmail.com>
2017-08-24 03:23:07 +0200

    problem 12478

---
+++ hardest_problem_ever_easy.cpp
@@ -0,0 +1,19 @@
+/*
+ * Competitive Programming
+ *
+ * @author        Daniele Cappuccio
+ * @link          (https://github.com/daniele-cappuccio/UVa-online-judge)
+ * @license       MIT License (https://opensource.org/licenses/MIT)
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    ios::sync_with_stdio(false); //faster I/O
+    cout << "KABIR" << endl;
+
+    return 0;
+}
