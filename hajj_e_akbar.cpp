c83fca0affd89822c1a32417a4856c08545cabbb
Daniele Cappuccio <daniele97bsk@gmail.com>
2017-09-01 00:49:45 +0200

    problem 12577

--- hajj_e_akbar.cpp
+++ hajj_e_akbar.cpp
@@ -1,3 +1,10 @@
+/*
+ * Competitive Programming
+ *
+ * @author        Daniele Cappuccio
+ * @link          (https://github.com/daniele-cappuccio/UVa-online-judge)
+ * @license       MIT License (https://opensource.org/licenses/MIT)
+ */
 
 #include <iostream>
 #include <string>
