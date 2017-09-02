044db8e6186f50aab9c664a17ac23ece8b415cb4
daniele-cappuccio <daniele97bsk@gmail.com>
2017-09-02 02:14:01 +0200

    Solution to problem 138 - Street Numbers

---
+++ 138 - Street Numbers.cpp
@@ -0,0 +1,28 @@
+/*
+ * Competitive Programming
+ *
+ * @author        Daniele Cappuccio
+ * @link          (https://github.com/daniele-cappuccio/UVa-online-judge)
+ * @license       MIT License (https://opensource.org/licenses/MIT)
+ */
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){    
+    
+    cout << setw(10) << 6 << setw(10) << 8 << endl;
+    cout << setw(10) << 35 << setw(10) << 49 << endl;
+    cout << setw(10) << 204 << setw(10) << 288 << endl;
+    cout << setw(10) << 1189 << setw(10) << 1681 << endl;
+    cout << setw(10) << 6930 << setw(10) << 9800 << endl;
+    cout << setw(10) << 40391 << setw(10) << 57121 << endl;
+    cout << setw(10) << 235416 << setw(10) << 332928 << endl;
+    cout << setw(10) << 1372105 << setw(10) << 1940449 << endl;
+    cout << setw(10) << 7997214 << setw(10) << 11309768 << endl;
+    cout << setw(10) << 46611179 << setw(10) << 65918161 << endl;
+    
+    return 0;
+}
