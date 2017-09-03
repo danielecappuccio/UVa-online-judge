989a44612f09b6db8b8b9ab2d19a519c901a9b37
daniele-cappuccio <daniele97bsk@gmail.com>
2017-09-03 12:35:32 +0200

    Solution to problem 10300 - Ecological Premium

---
+++ 10300 - Ecological Premium.cpp
@@ -0,0 +1,25 @@
+ /**
+ * Competitive Programming
+ *
+ * @author        Daniele Cappuccio
+ * @link          (https://github.com/daniele-cappuccio/UVa-online-judge)
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int T, S, A, E, n, b;
+    cin >> T;
+    for (int t=0; t<T; ++t){
+        cin >> n;
+        b = 0;
+        for (int i=0; i<n; ++i){
+            cin >> S >> A >> E;
+            b += S*E;
+        }
+        cout << b << endl;
+    }
+    return 0;
+}
