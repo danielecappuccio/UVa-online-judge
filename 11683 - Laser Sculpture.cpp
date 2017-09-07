d9b67e51c12ae81391b1e9d54806feb54a3ee57b
daniele-cappuccio <daniele97bsk@gmail.com>
2017-09-07 21:57:24 +0200

    Solution to problem 11683 - Laser Sculpture

---
+++ 11683 - Laser Sculpture.cpp
@@ -0,0 +1,42 @@
+/*
+ * Competitive Programming
+ *
+ * @author        Daniele Cappuccio
+ * @link          (https://github.com/daniele-cappuccio/UVa-online-judge)
+ * @license       MIT License (https://opensource.org/licenses/MIT)
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    ios_base::sync_with_stdio(false); // faster I/O
+
+    int A, C, height, max = 0, last = 0, cut, i;
+    cin >> A;
+
+    do {
+        cin >> C;
+
+        cut = 0;
+        last = A;
+        
+        for (i = 0; i < C; ++i) {
+            cin >> height;
+            if (height >= last) {
+                last = height;
+                continue;
+            }
+            cut += last - height;
+            last = height;
+        }
+        cout << cut << endl;
+
+        cin >> A;
+
+    } while (A != 0);
+
+    return 0;
+}
